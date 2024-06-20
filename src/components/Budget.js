2e4fa3ecf0c8285bb44864139801e94d234ae830
AndyZhang17 <andytb17@gmail.com>
2024-06-20 18:02:27 +0800

    finish final project

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
+import React, {useContext, useState} from 'react';
+import {AppContext} from '../context/AppContext';
 
+const Budget = () => {
+    const {currency, budget, dispatch} = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        const newvalue = event.target.value;
+        dispatch({type: "SET_BUDGET", payload: event.target.value});
+        setNewBudget(newvalue);
+    }
+
+/* 
+Here, you are using the Bootstrap Alert classes to give a nice 
+gray background by adding some text and hard coding a value.
+*/
+    return (
+<div className = "alert alert-secondary">
+    <span>Budget: {currency}{newBudget}</span>
+    <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+
+export default Budget;
